98c018db0506c46377ecffaaba3c12fc8e191331
DesertStormII <119744433+DesertStormII@users.noreply.github.com>
2023-01-03 17:08:01 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+
+using  namespace std;
+
+int main()
+{
+	
+	int n=1, p = 1000, r = 5;
+	cout << "your benefices after 10 years --- ";
+	while (n <= 10) {
+		p = ((p / 100) * r) + p;
+		n++;
+		cout << p << endl;
+	}
+	cout << p;
+
+
+
+
+	return 0;
+}
